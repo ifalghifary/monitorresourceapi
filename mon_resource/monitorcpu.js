48e6447561bce19512a7d453ad6ab087f650546a
unknown <ifalgrafi@gmail.com>
2022-11-04 09:11:49 +0700

    initial

---
+++ mon_resource/monitorcpu.js
@@ -0,0 +1,44 @@
+var os = require('os-utils')
+var os2 = require('os')
+const checkDiskSpace = require('check-disk-space').default
+var fs = require('fs')
+
+// fs.readFile('temp.json', function (err, buf) {
+//   console.log(buf.toString())
+// })
+
+fs.readFile('data.json', 'utf-8', (err, data) => {
+  if (err) {
+    // console.log(err)
+    fs.writeFile('data.json', `{ "tes": "tes" }`, (err) => {
+      if (err) console.log(err)
+      //   console.log('Successfully Written to File.')
+    })
+  }
+  console.log(data)
+})
+var disk = checkDiskSpace('C:/blabla/bla').then((diskSpace) => {
+  return diskSpace
+})
+async function mrmr() {
+  return os.cpuUsage(async (v) => {
+    // console.log(v)
+    Promise.all([disk, os2.freemem() / 1000000000, v]).then((x) => {
+      let data = {
+        disk: x[0],
+        ram: x[1],
+        cpu: x[2],
+      }
+      let final = JSON.stringify(data)
+
+      fs.writeFile('data.json', final, (err) => {
+        if (err) console.log(err)
+        //   console.log('Successfully Written to File.')
+      })
+    })
+  })
+}
+
+setInterval(() => {
+  mrmr()
+}, 5000)
